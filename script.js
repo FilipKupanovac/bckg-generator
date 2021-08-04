8389f89f989ec5b186f188ef417b34f83cf83441
FilipKupanovac <80569573+FilipKupanovac@users.noreply.github.com>
2021-08-04 16:21:03 +0200

    Added .gitgnore

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var _lodash = require('lodash');
+
+var array = [1,2,3,4,5];
+console.log('answer', _lodash.without(array,3));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
